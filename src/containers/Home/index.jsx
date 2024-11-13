1025435a5c8fe4ea4ea3bb7df568c7b23642641a
gabriellima11 <gabrielflima05@gmail.com>
2024-11-13 14:54:52 -0300

    validate token in Menu

--- src/containers/Home/index.jsx
+++ src/containers/Home/index.jsx
@@ -1,9 +1,21 @@
 import { Container, Banner, Content } from './style';
 
+import { useNavigate } from 'react-router-dom';
 import HomeBanner from '../../assets/banner-home.svg';
 import { CategoryCarousel, OffersCarousel } from '../../components';
+import { useEffect } from 'react';
 
 export const Home = () => {
+  const navigate = useNavigate();
+  const token = userData && JSON.parse(userData).token;
+
+  useEffect(() => {
+    if (!token) {
+      navigate('/login');
+      return null;
+    }
+  }, []);
+
   return (
     <main>
       <Banner>
